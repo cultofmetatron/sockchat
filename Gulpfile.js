28bd5308ef0fc274523a0ae6242bea6f9c0c248f
Peter de Croos <cultofmetatron@aumlogic.com>
2015-03-08 18:16:38 -0700

    hooked in less compiler

--- Gulpfile.js
+++ Gulpfile.js
@@ -3,8 +3,10 @@ var gutil = require('gulp-util');
 //var webpack = require('webpack');
 var WebpackDevServer = require('webpack-dev-server');
 var webpack = require('gulp-webpack');
+var less = require('gulp-less');
+var path = require('path');
 
-gulp.task('webpack', function(cb) {
+gulp.task('webpack', function() {
   return gulp.src('./frontend/src/js/index.js')
     .pipe(webpack({
       output: {
@@ -15,6 +17,15 @@ gulp.task('webpack', function(cb) {
 });
 
 
+gulp.task('less', function() {
+  return gulp.src('./frontend/src/stylesheets/**/*.less')
+    .pipe(less({
+      paths: [ path.join(__dirname,'frontend', 'src', 'stylesheets') ]
+    }))
+    .on('error', logger)
+    .pipe(gulp.dest('./frontend/build/stylesheets'))
+});
+
 
 gulp.task('default', function() {
 
@@ -22,6 +33,13 @@ gulp.task('default', function() {
 });
 
 
+
+
+
+
+function logger() {
+  return console.log.apply(console, arguments);
+}
 
 
 
